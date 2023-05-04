d86e27ee40a0f1430e572b41a402d8232c28977c
Leonardo Santos <leonardojosantos@gmail.com>
2023-05-04 15:33:10 -0300

    A sintaxe PHP.

---
+++ Desenvolvimento_Web_em_Html5_CSS_Javascript_e_PHP/Tema_5-Programacao_de_Paginas_Dinamicas_com_PHP/02-Termino_de_instrucoes_e_comentarios/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+        //  Comentários de uma linha são realizados com a utilização de barras duplas
+
+        /*
+            Cada comando deve ser terminado com ;
+            Abaixo temos alguns exemplos de término de comandos
+        */
+
+        echo "Olá, mundo\n";
+        $var1 = 2 + 2;
+        echo "O Resultado da soma é igual a: " . $var1;
+    ?>
+
+</body>
+
+</html>
