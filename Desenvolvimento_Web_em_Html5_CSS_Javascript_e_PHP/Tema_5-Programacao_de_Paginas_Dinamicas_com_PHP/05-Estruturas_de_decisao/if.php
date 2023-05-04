03821e883ec208acb59bc778456842ef36ccad73
Leonardo Santos <leonardojosantos@gmail.com>
2023-05-04 19:37:27 -0300

    Condicionais: Estruturas de decisão

---
+++ Desenvolvimento_Web_em_Html5_CSS_Javascript_e_PHP/Tema_5-Programacao_de_Paginas_Dinamicas_com_PHP/05-Estruturas_de_decisao/if.php
@@ -0,0 +1,8 @@
+<?php
+
+$var1 = 120;
+$var2 = 20;
+
+if ($var1 > $var2) {
+    echo $var1 . ' é maior que ' . $var2;
+}
---
+++ Desenvolvimento_Web_em_Html5_CSS_Javascript_e_PHP/Tema_5-Programacao_de_Paginas_Dinamicas_com_PHP/05-Estruturas_de_decisao/switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$var1 = 10;
+
+switch ($var1) {
+    case 10:
+        echo $var1 . ' é igual a 10';
+        break;
+
+    case 20:
+        echo $var1 . ' é igual a 20';
+        break;
+
+    default:
+        echo $var1 . ' não é igual a 10 nem igual a 20';
+        break;
+}
