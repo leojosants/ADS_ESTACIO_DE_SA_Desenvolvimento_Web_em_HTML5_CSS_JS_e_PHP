b12da03e7f6cbee3513a900f7b0032acbe36fcc6
Leonardo Santos <leonardojosantos@gmail.com>
2023-05-05 15:08:22 -0300

    Arrays: vetores

---
+++ Desenvolvimento_Web_em_Html5_CSS_Javascript_e_PHP/Tema_5-Programacao_de_Paginas_Dinamicas_com_PHP/07-Arrays-vetores/Pratica_01/index.php
@@ -0,0 +1,25 @@
+<?php
+$frutas = array(
+    "vermelhas" => array(
+        "melancia",
+        "cereja",
+        "framboesa",
+        "morango"
+    ),
+
+    "citricas" => array(
+        "laranja",
+        "limao",
+        "abacaxi",
+        "mexerica"
+    ),
+
+    "tropicais" => array(
+        "goiaba",
+        "maracujá",
+        "banana",
+        "manga"
+    ),
+);
+
+print_r($frutas);
---
+++ Desenvolvimento_Web_em_Html5_CSS_Javascript_e_PHP/Tema_5-Programacao_de_Paginas_Dinamicas_com_PHP/07-Arrays-vetores/array_tipo_misto.php
@@ -0,0 +1,23 @@
+<?php
+$carros = array(
+    'vw'        => "Fusca",
+    0           => "Passat",
+    'chevrolet' => "Monza",
+    1           => "Chevette",
+    'fiat'      => "Tempra",
+    2           => "Uno"
+);
+
+ //A impressão desse array gera a seguinte saída
+ /*
+ Array
+ (
+	[vw] => Fusca
+	[0] => Passat
+	[chevrolet] => Monza
+	[1] => Chevette
+	[fiat] => Tempra
+	[2] => Uno
+ )
+ */
+print_r($carros); 
