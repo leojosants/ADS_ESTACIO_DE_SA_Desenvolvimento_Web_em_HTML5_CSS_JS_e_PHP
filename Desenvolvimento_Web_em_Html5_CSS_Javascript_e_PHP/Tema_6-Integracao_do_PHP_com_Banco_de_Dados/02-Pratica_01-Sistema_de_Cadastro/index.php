bb918e63518d65fd7a818a33c98801becf32aa26
Leonardo Santos <leonardojosantos@gmail.com>
2023-05-06 14:51:44 -0300

    Criação da página inicial.

---
+++ Desenvolvimento_Web_em_Html5_CSS_Javascript_e_PHP/Tema_6-Integracao_do_PHP_com_Banco_de_Dados/02-Pratica_01-Sistema_de_Cadastro/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Olá, seja bem-vindo(a)!</h1>
+
+        <form class="" action="" method="post">
+            <div class="col-4">
+                <label for="email">E-mail</label>
+                <input type="email" name="email" id="email" class="form-control" />
+            </div>
+
+            <div class="col-4">
+                <label for="senha">Senha</label>
+                <input type="password" name="senha" id="senha" class="form-control" />
+            </div> <br />
+
+            <button type="submit" name="enviarDados" class="btn btn-primary">Entrar</button>
+
+            <a href="#" class="btn btn-warning">Cadastrar-se</a>
+        </form>
+    </div>
+</body>
+
+</html>
